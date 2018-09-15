9816b02bc57da62eb241cf534420edc1fa41ae3a
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2018-09-15 16:57:15 +0200

    changed table router from clssic to regular expression table

--- public/index.php
+++ public/index.php
@@ -4,14 +4,15 @@ require_once '../app/core/Router.php';
 
 $oRouter = new Router();
 
-$oRouter->add('',['controller'=>'Home','action'=>'index']);
-$oRouter->add('posts',['controller'=>'Posts','action'=>'index']);
-$oRouter->add('posts/new',['controller'=>'Posts','action'=>'new_post']);
+// $oRouter->add('',['controller'=>'Home','action'=>'index']);
+// $oRouter->add('posts',['controller'=>'Posts','action'=>'index']);
+// $oRouter->add('posts/new',['controller'=>'Posts','action'=>'new_post']);
+$oRouter->add('{controller}/{action}');
+$oRouter->add('{controller}/{id:\d+}/{action}');
 
 $url = $_SERVER['QUERY_STRING'];
-if ($oRouter->is_matched($url)){
+if ($oRouter->is_matched($url)) {
     var_dump($oRouter->get_parameters());
-}
-else{
+} else {
     echo "Site not found for: " . $url;
 }
