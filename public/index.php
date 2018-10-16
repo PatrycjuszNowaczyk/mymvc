5d2b0dce2622c58f5c13cc312956fbd5ea48da21
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2018-10-16 16:30:08 +0200

    Experimenting with deleting debug.log file

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
 
-/* *
- *This MVC is made thanks Dave Hollingworth tutorial:
- *WRITE PHP LIKE A PRO: BUILD A PHP MVC FRAMEWORK FROM SCRATCH
- */
+/***************************************************************/
+/*This MVC is made thanks Dave Hollingworth tutorial:          */
+/*WRITE PHP LIKE A PRO: BUILD A PHP MVC FRAMEWORK FROM SCRATCH */
+/***************************************************************/
 
-/*autoloader classes*/
+/*autoloader controllers*/
 spl_autoload_register(function ($sClass) {
     $sRoot = dirname(__DIR__);
     $sFile = $sRoot . '/' . str_replace('\\', '/', $sClass) . '.php';
